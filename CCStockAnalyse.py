6aaf0f99f739fd76b052f7070e8874be5880ad8a
CC <ccworld1000@gmail.com>
2017-10-30 04:22:14 +0800

    Param

--- CCStockAnalyse.py
+++ CCStockAnalyse.py
@@ -26,12 +26,18 @@
 
 import re
 import os
+import time
 import json
 import requests
 
-
-
-class CCParm (object) :
+def getTimestamp () :
+	ts = time.time()
+	ts1000 =  str(long (float(ts) * 1000))
+	print str(ts1000)
+		
+	return ts1000
+		
+class CCParam (object) :
 	def __init__ (self, stock_code, step, start, count, timestamp) :
 		self.stock_code = stock_code
 		self.step = step;
@@ -44,8 +50,28 @@ class CCParm (object) :
 		self.vv=100
 		self.fq_type='no'
 
+	def convertParam(self) :
+		index = 1;
+		strings = []
+		for k, v in self.__dict__.items() :
+			paris = "{}={}".format(k, v)
+			print "[CC " + str(index) + "] " + paris
 
-	def genHeaders (url) :
+			strings.append(paris)
+
+			index = index + 1
+		
+		strings.append("from=pc")
+		strings.append("format=json")
+		
+		retString = "&".join (strings)
+			
+		print retString
+		
+		return retString
+		
+
+	def genHeaders (self, url) :
 		host = url
 		if url :
 			match = re.compile (':\/\/(.*?com)')
@@ -58,19 +84,23 @@ class CCParm (object) :
 			'Accept-Encoding':'gzip, deflate',
 			'Accept-Language':'en-US,en;q=0.8',
 			'Connection':'keep-alive',
-			'Cookie':'UM_distinctid=15f437b4b97121-042a2340cadd34-31607c03-fa000-15f437b4b98a8; Hm_lvt_9a737a8572f89206db6e9c301695b55a=1508665672; Hm_lpvt_9a737a8572f89206db6e9c301695b55a=1508706346',
+			'Cookie':'IDUPSID=9A83A4241A43A21C91557E6DE8AA8709; PSTM=1508161587; BAIDUID=300644275A35780F55B642CE762700A7:FG=1; PSINO=6; H_PS_PSSID=24875_1454_24886_21099_18559_23384_22159; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; Hm_lvt_35d1e71f4c913c126b8703586f1d2307=1509225661,1509226500,1509302137,1509307401; Hm_lpvt_35d1e71f4c913c126b8703586f1d2307=1509307413',
 			'Host': host,
-			'Referer':url,
+			#'Referer':url,
 			'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 		}
 		
 		return headers
 	
-	def getSite (url, rootdir) :
-		headers = genHeaders(url)
+	def getSite (self, url, param) :
+		headers = self.genHeaders(url)
 		requests.Session()
-		r = requests.get (url, headers = headers)
+		address = "{}?{}".format(url, param)
+		print address
+		r = requests.get (address, headers = headers)
 		r.encoding = r.apparent_encoding
 		html = r.text
+		
+		print html
 
 
